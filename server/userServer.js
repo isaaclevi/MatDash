7701d3543dd6c1331db74d69fa00178196439551
isaaclevi <isaaclevi1710@gmail.com>
2019-07-03 16:12:12 -0400

    first

---
+++ server/userServer.js
@@ -0,0 +1,143 @@
+//const Joi = require('joi');
+const cors = require('cors');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const port = normalizePort(process.env.PORT || '3000');
+const User = require(__dirname+'/models/Users');
+const config = {
+  autoIndex: false,
+  useNewUrlParser: true,
+};
+//const favicon = require('serve-favicon');
+//const logger = require('morgan');
+
+//const apiRouter = require('./server/routes/Users.js');
+//const dashRouter = require('./routes/Dashboard');
+
+const mongoose = require('mongoose');
+const mongodbHost = 'ds163650.mlab.com';
+const mongodbPort = '63650';
+const authenticate = 'ApiAccess:mazel098@'; //admin 'yona321:mazel1419@'
+const mongodbDatabase = 'compudime';
+
+// connect string for mongodb server running locally, connecting to a database called test
+const url = 'mongodb://'+authenticate+mongodbHost+':'+mongodbPort + '/' + mongodbDatabase;
+
+const server = express();
+server.use(cors());
+server.set('port', port);
+//server.setHeader({'Access-Control-Allow-Origin': '*','Content-Type': 'application/json'});
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
+
+mongoose.connect(url, { useNewUrlParser: true,promiseLibrary: require('bluebird') })
+  .then(() =>  console.log('connection successful'))
+  .catch((err) => console.error(err));
+
+  //get all users
+server.get('/', (req, res, next)=> {
+  User.find((err, products)=> {
+    if (err) return next(err);
+    res.set();
+    res.json(products);
+  });
+});
+
+//get a user by id
+server.get('/:id', function(req, res, next) {
+  User.findById(req.params.id, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+//server.use(logger('dev'));
+//server.use(express.json());
+//server.use(express.urlencoded({ extended: false }));
+//server.use(express.static(path.join(__dirname, 'dist/dashboardServer')));
+//server.use('/', express.static(path.join(__dirname, 'dist/dashboardServer')));
+//server.use('/api', apiRouter);
+
+// catch 404 and forward to error handler
+server.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+server.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.send(err.status);
+});
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+module.exports = server;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
+import { RouterModule,Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule, MatGridListModule, MatCardModule, MatMenuModule, MatIconModule, MatToolbarModule, MatSidenavModule, MatListModule,MatNativeDateModule} from '@angular/material';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { DashComponent } from './dash/dash.component';
 
+const appRoutes: Routes = [
+  {path:'1',component:DashComponent}
+  /*{path:'2',component:},
+  {path:'3',component:}*/
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    DashComponent,
   ],
+
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes),
+    MatButtonModule, MatCheckboxModule, MatGridListModule, MatCardModule, MatMenuModule, MatIconModule, LayoutModule, MatToolbarModule, MatSidenavModule, MatListModule,MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+export class PizzaPartyAppModule { }
